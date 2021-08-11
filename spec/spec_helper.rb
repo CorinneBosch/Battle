6f011b597082b9fd7526d1afcee633b3a6101d27
Corinne <corinne.boesch@gmail.com>
2021-08-11 15:35:00 +0100

    test infra spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__File__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
