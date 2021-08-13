1e700a08ba08d8981000739057ffcef03d31f5f2
edpackard <edpackard81@gmail.com>
2021-08-13 16:02:28 +0100

    Finished extracting Player name logic to the model

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'player'
 require 'sinatra/base'
 require 'sinatra/reloader'
 
@@ -14,14 +15,12 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:first_player] = params[:first_player]
-    session[:second_player] = params[:second_player]
+    $first_player = Player.new(params[:first_player])
+    $second_player = Player.new(params[:second_player])
     redirect '/play'
   end
 
   get '/play' do
-    @first_player = session[:first_player]
-    @second_player = session[:second_player]
     @health1 = 60
     @health2 = 60
     @default_health = 60
@@ -29,8 +28,6 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @first_player = session[:first_player]
-    @second_player = session[:second_player]
     erb :attack
   end
 
--- spec/units/player_spec.rb
+++ spec/units/player_spec.rb
@@ -2,7 +2,6 @@ require 'player'
 
 describe Player do
   it 'returns name' do
-    
     expect(subject.name).to eq "Frosmoth"
   end
 
